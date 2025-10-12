88f870da254171409dfc9f1d77d1fb71342d31ae
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2025-10-12 14:03:35 +0200

    Add utils and class Node into

---
+++ extract_rules/utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+class Node:
+    """
+    Tree node class
+
+    Parameters
+    ----------
+    feature : int
+        Current node feature indice splitting
+    treshold : flaot
+        Current node treshold splittting
+    side : str
+        Side of the rule. 'L' for Left (i.e. less or equal) and 'R' for right (i.e. gretter)
+    node_id : int
+       Current  Node id
+    children : list of Node
+        Child Nodes if not a leaf node
+    Returns
+    ----------
+    Node: Node
+        The current Node instance
+    """
+
+    def __init__(self, feature=None, treshold=-1, side=None, node_id=-1, *children):
+        self.node_id = node_id
+        self.feature = feature
+        self.treshold = treshold
+        self.side = side
+        if children:
+            self.children = children
+        else:
+            self.children = []
