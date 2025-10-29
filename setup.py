f66767cb75d30e45fc7f28fc9ec728fec3ab94e4
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2025-10-29 15:07:57 +0100

    Update pypitest version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extensions = [
 
 setup(
     name="woodtapper",  # must be unique on PyPI
-    version="0.0.2",
+    version="0.0.4",
     author="Abdoulaye SAKHO",
     author_email="abdoulaye.sakho@artefact.com",
     description="A Python toolbox for interpretable and explainable tree ensembles.",
