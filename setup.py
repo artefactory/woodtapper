5579dba6161ce711fa563bd2f936071030807d7e
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2025-10-30 15:05:12 +0100

    Update v0.0.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extensions = [
 
 setup(
     name="woodtapper",  # must be unique on PyPI
-    version="0.0.7",
+    version="0.0.8",
     author="Abdoulaye SAKHO",
     author_email="abdoulaye.sakho@artefact.com",
     description="A Python toolbox for interpretable and explainable tree ensembles.",
