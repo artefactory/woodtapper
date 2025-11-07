6252a11e720678f2b16a7c9f85c2852a39bd9b9d
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2025-11-07 19:43:34 +0100

    update test for rules extractors

--- tests/test_unit_extractor.py
+++ tests/test_unit_extractor.py
@@ -11,30 +11,30 @@ def test_sirus_fit(simple_dataset, simple_regression_data):
     X, y = simple_dataset
     X_reg, y_reg = simple_regression_data
 
-    model = SirusClassifier(n_estimators=100, p0=0.0, num_rule=5)
+    model = SirusClassifier(n_estimators=100, p0=0.0, max_n_rules=5)
     model.fit(X, y)
     assert len(model.estimators_) > 0
-    assert model.num_rule > 0
+    assert model.max_n_rules > 0
 
-    model_regressor = SirusRegressor(n_estimators=100, p0=0.0, num_rule=5)
+    model_regressor = SirusRegressor(n_estimators=100, p0=0.0, max_n_rules=5)
     model_regressor.fit(X_reg, y_reg)
     assert len(model_regressor.estimators_) > 0
-    assert model_regressor.num_rule > 0
+    assert model_regressor.max_n_rules > 0
 
-    model_gb_clf = GbExtractorClassifier(n_estimators=100, p0=0.0, num_rule=5)
+    model_gb_clf = GbExtractorClassifier(n_estimators=100, p0=0.0, max_n_rules=5)
     model_gb_clf.fit(X, y)
     assert len(model_gb_clf.estimators_) > 0
-    assert model_gb_clf.num_rule > 0
+    assert model_gb_clf.max_n_rules > 0
 
-    model_gb_regressor = GbExtractorRegressor(n_estimators=100, p0=0.0, num_rule=5)
+    model_gb_regressor = GbExtractorRegressor(n_estimators=100, p0=0.0, max_n_rules=5)
     model_gb_regressor.fit(X_reg, y_reg)
     assert len(model_gb_regressor.estimators_) > 0
-    assert model_gb_regressor.num_rule > 0
+    assert model_gb_regressor.max_n_rules > 0
 
 
 def test_sirus_fit_sets_attributes(simple_dataset):
     X, y = simple_dataset
-    model = SirusClassifier(n_estimators=50, num_rule=3)
+    model = SirusClassifier(n_estimators=50, max_n_rules=3)
     model.fit(X, y)
     assert hasattr(model, "rules_"), "SIRUS should store extracted rules"
     assert isinstance(model.rules_, list)
