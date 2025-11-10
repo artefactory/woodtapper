e25843ace4f1dbb50039ea3d71e57a21b23f90a1
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2025-11-10 18:00:14 +0100

    ADD gb explanation classes

--- woodtapper/example_sampling/__init__.py
+++ woodtapper/example_sampling/__init__.py
@@ -3,15 +3,19 @@
 from .classification_explanation import (
     RandomForestClassifierExplained,
     ExtraTreesClassifierExplained,
+    GradientBoostingClassifierExplained,
 )
 from .regression_explanation import (
     RandomForestRegressorExplained,
     ExtraTreesRegressorExplained,
+    GradientBoostingRegressorExplained,
 )
 
 __all__ = [
     "RandomForestClassifierExplained",
     "ExtraTreesClassifierExplained",
+    "GradientBoostingClassifierExplained",
     "RandomForestRegressorExplained",
     "ExtraTreesRegressorExplained",
+    "GradientBoostingRegressorExplained",
 ]
