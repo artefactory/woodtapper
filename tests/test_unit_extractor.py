336ce5a98e59953b8ee76b2278d1dfbbe8f5913f
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2025-11-12 15:54:43 +0100

    ADD unit test on show_rules

--- tests/test_unit_extractor.py
+++ tests/test_unit_extractor.py
@@ -1,3 +1,6 @@
+import io
+import sys
+
 import numpy as np
 from woodtapper.extract_rules import (
     SirusClassifier,
@@ -7,6 +10,7 @@ from woodtapper.extract_rules import (
     GbExtractorClassifier,
     GbExtractorRegressor,
 )
+from woodtapper.extract_rules.visualization import show_rules
 
 
 def test_sirus_fit(simple_dataset, simple_regression_data):
@@ -64,3 +68,44 @@ def test_predict_value_range(trained_sirus_on_simple, simple_dataset):
     X, _ = simple_dataset
     preds = trained_sirus_on_simple.predict(X)
     assert np.all((preds >= 0) & (preds <= 1)), "Predictions should be probabilities"
+
+
+def test_rules_vizualization(simple_dataset, simple_regression_data):
+    X, y = simple_dataset
+    X_reg, y_reg = simple_regression_data
+
+    model = SirusClassifier(n_estimators=100, p0=0.0, max_n_rules=5)
+    model.fit(X, y)  # Capture printed output
+    captured_output = io.StringIO()
+    sys.stdout = captured_output  # Call the function
+    show_rules(
+        model, max_rules=3, is_regression=False, target_class_index=1
+    )  # Reset stdout
+    sys.stdout = sys.__stdout__  # Check the result
+    output = captured_output.getvalue().strip()
+    expected_output = """Estimated average rate for target class 1 (from 'else' clauses) p_s = 53%.
+(Note: True average rate should be P(Class=1) from training data).
+
+IF Condition               THEN P(C1)      ELSE P(C1)
+----------------------------------------------------------
+if   Feature[0] <= 0.42    then 20%                else 90%
+if   Feature[0] > -0.29    then 75%                else 8%
+if   Feature[4] <= -0.77   then 75%                else 41%"""
+    assert output == expected_output, f"Unexpected output: {output}"
+
+    model_regressor = SirusRegressor(
+        n_estimators=100, p0=0.0, max_n_rules=5, random_state=0
+    )
+    model_regressor.fit(X_reg, y_reg)
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    show_rules(model_regressor, max_rules=3, is_regression=True)
+    sys.stdout = sys.__stdout__
+    output = captured_output.getvalue().strip()
+    print(output)
+    expected_output = """IF Condition              THEN P(C1)      ELSE P(C1)
+---------------------------------------------------------
+if   Feature[1] <= 0.53   then -1.33              else 4.02
+if   Feature[1] <= 0.34   then -19.55             else 37.07
+if   Feature[1] > -0.51   then 36.21              else -65.94"""
+    assert output == expected_output, f"Unexpected output: {output}"
