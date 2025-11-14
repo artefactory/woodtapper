36d1b11d4ab0dd1e9e3b02b2479df04f699c58b0
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2025-11-14 09:43:31 +0100

    ADD python 3.11 compatibility

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extensions = [
 
 setup(
     name="woodtapper",  # must be unique on PyPI
-    version="0.0.10",
+    version="0.0.11",
     author="Abdoulaye SAKHO",
     author_email="abdoulaye.sakho@artefact.com",
     description="A Python toolbox for interpretable and explainable tree ensembles.",
