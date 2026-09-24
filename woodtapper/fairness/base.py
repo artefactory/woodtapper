4687d2a1a648e45aa9b5c9b281e0114a0b9b2ceb
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2026-09-24 18:59:14 +0200

    feat: add gradient wé derivation when sensitive attribute is multiclass

--- woodtapper/fairness/base.py
+++ woodtapper/fairness/base.py
@@ -5,7 +5,7 @@ from scipy.stats import cumfreq
 from woodtapper.fairness.utils import _invert_cdf, _split_groups
 
 
-def w2_fair_grad_binary(y_preds, sensitive_attribute, n_steps_cdf):
+def w2_fair_grad_binary(y_preds, sensitive_attribute, n_steps_cdf=1024):
     """
     Compute the Wasserstein-2 fair gradient for binary sensitive attributes.
 
@@ -23,12 +23,12 @@ def w2_fair_grad_binary(y_preds, sensitive_attribute, n_steps_cdf):
     grad : np.ndarray
         Gradient adjusted for fairness.
     """
-    s = np.asarray(sensitive_attribute, dtype=int)
+    sensitive_attribute = np.asarray(sensitive_attribute, dtype=int)
     n = len(y_preds)
-    n0 = np.sum(s == 0)
-    n1 = np.sum(s == 1)
+    n0 = np.sum(sensitive_attribute == 0)
+    n1 = np.sum(sensitive_attribute == 1)
 
-    idx0, idx1, y0, y1 = _split_groups(y_preds, s)
+    idx0, idx1, y0, y1 = _split_groups(y_preds, sensitive_attribute)
     if y0.size == 0 or y1.size == 0:
         return np.zeros(n, dtype=float)
     array_steps_eta, eta_step = np.linspace(y_preds.min(), y_preds.max(), n_steps_cdf + 1, retstep=True)
@@ -49,4 +49,26 @@ def w2_fair_grad_binary(y_preds, sensitive_attribute, n_steps_cdf):
  
     grad[idx0] = (2 * (y0 - cor1_y0)) / n0
     grad[idx1] = (2 * (y1 - cor0_y1)) / n1
+    return grad
+
+def w2_fair_grad(y_preds, sensitive_attribute, n_steps_cdf=1024):
+    sensitive_attribute = np.asarray(sensitive_attribute, dtype=int)
+    n = len(y_preds)
+    #n0 = np.sum(sensitive_attribute == 0)
+    #n1 = np.sum(sensitive_attribute == 1)
+    classes = np.unique(sensitive_attribute)
+    grad = np.zeros(n, dtype=float)
+    if classes.size < 2:
+        return grad
+
+    if classes.size == 2:
+        s_bin = np.where(sensitive_attribute == classes[0], 0, 1)
+        return w2_fair_grad_binary(y_preds, s_bin, n_steps_cdf=n_steps_cdf)
+
+    for group in classes:
+        s_ovr = np.where(sensitive_attribute == group, 0, 1)
+        grad_ovr = w2_fair_grad_binary(y_preds, s_ovr, n_steps_cdf=n_steps_cdf)
+        idx_group = np.where(sensitive_attribute == group)[0]
+        grad[idx_group] = grad_ovr[idx_group]
+
     return grad
