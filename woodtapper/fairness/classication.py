d01d47229eadb8d0f450fa38c60e04df3489c814
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2026-09-25 17:07:22 +0200

    feat: OT classifier simple implem based on heritage

--- woodtapper/fairness/classication.py
+++ woodtapper/fairness/classication.py
@@ -1,3 +1,66 @@
-class OTFairBoostClassifier:
-    def __init__(self):
-        pass
+import lightgbm as lgb
+
+from .objective import build_fair_loss
+
+
+class OTFairBoostClassifier(lgb.LGBMClassifier):
+    def __init__(
+        self,
+        lambda_fairness_value,
+        n_steps_cdf=1024,
+        fairness_mode="Demographic_Parity",
+        boosting_type="gbdt",
+        num_leaves=31,
+        max_depth=-1,
+        learning_rate=0.1,
+        n_estimators=100,
+        subsample_for_bin=200000,
+        objective=None,
+        class_weight=None,
+        min_split_gain=0.0,
+        min_child_weight=1e-3,
+        min_child_samples=20,
+        subsample=1.0,
+        subsample_freq=0,
+        colsample_bytree=1.0,
+        reg_alpha=0.0,
+        reg_lambda=0.0,
+        random_state=None,
+        n_jobs=None,
+        importance_type="split",
+    ):
+        super().__init__(
+            boosting_type=boosting_type,
+            num_leaves=num_leaves,
+            max_depth=max_depth,
+            learning_rate=learning_rate,
+            n_estimators=n_estimators,
+            subsample_for_bin=subsample_for_bin,
+            objective=objective,
+            class_weight=class_weight,
+            min_split_gain=min_split_gain,
+            min_child_weight=min_child_weight,
+            min_child_samples=min_child_samples,
+            subsample=subsample,
+            subsample_freq=subsample_freq,
+            colsample_bytree=colsample_bytree,
+            reg_alpha=reg_alpha,
+            reg_lambda=reg_lambda,
+            random_state=random_state,
+            n_jobs=n_jobs,
+            importance_type=importance_type,
+        )
+        self.lambda_fairness_value = lambda_fairness_value
+        self.n_steps_cdf = n_steps_cdf
+        self.fairness_mode = fairness_mode
+
+    def fit(self, X, y, sensitive_attribute):
+        fobj = build_fair_loss(
+            sensitive_attribute=sensitive_attribute,
+            lambda_fairness_value=self.lambda_fairness_value,
+            fairness_mode=self.fairness_mode,
+            n_steps_cdf=self.n_steps_cdf,
+            is_classification=True,
+        )
+        self.objective = fobj
+        return super().fit(X, y)
