be2f375524804ef69f294c77b0ad20b15c172986
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2026-09-25 15:15:43 +0200

    fix: keep only one function for grand and hess with multiclass sensitive attribute

--- woodtapper/fairness/base.py
+++ woodtapper/fairness/base.py
@@ -5,9 +5,6 @@ from scipy.stats import cumfreq
 from woodtapper.fairness.utils import _invert_cdf, _split_groups
 
 
-####################################################################################
-###################################### Gradient ###################################
-####################################################################################
 def w2_fair_grad_binary(y_preds, sensitive_attribute, n_steps_cdf=1024):
     """
     Compute the Wasserstein-2 fair gradient for binary sensitive attributes.
@@ -60,35 +57,6 @@ def w2_fair_grad_binary(y_preds, sensitive_attribute, n_steps_cdf=1024):
     return grad
 
 
-def w2_fair_grad(y_preds, sensitive_attribute, n_steps_cdf=1024):
-    sensitive_attribute = np.asarray(sensitive_attribute, dtype=int)
-    n = len(y_preds)
-    classes = np.unique(sensitive_attribute)
-    grad = np.zeros(n, dtype=float)
-    if classes.size < 2:
-        return grad
-
-    if classes.size == 2:
-        return w2_fair_grad_binary(
-            y_preds, sensitive_attribute, n_steps_cdf=n_steps_cdf
-        )
-
-    for group in classes:
-        s_ovr = np.where(
-            sensitive_attribute == group, 0, 1
-        )  # group (as 0) vs rest (as 1)
-        grad_ovr = w2_fair_grad_binary(y_preds, s_ovr, n_steps_cdf=n_steps_cdf)
-        idx_group = np.where(sensitive_attribute == group)[0]
-        grad[idx_group] = grad_ovr[idx_group]
-
-    return grad
-
-
-####################################################################################
-####################################### Hessian ####################################
-####################################################################################
-
-
 def w2_fair_hess_binary(y_preds, sensitive_attribute):
     sensitive_attribute = np.asarray(sensitive_attribute, dtype=int)
     n = len(y_preds)
@@ -104,28 +72,6 @@ def w2_fair_hess_binary(y_preds, sensitive_attribute):
     h1 = 2 / n1
     hess[idx0] = h0
     hess[idx1] = h1
-
-    return hess
-
-
-def w2_fair_hess(y_preds, sensitive_attribute):
-    sensitive_attribute = np.asarray(sensitive_attribute, dtype=int)
-    n = len(y_preds)
-    classes = np.unique(sensitive_attribute)
-    hess = np.zeros(n, dtype=float)
-    if classes.size < 2:
-        return hess
-
-    if classes.size == 2:
-        return w2_fair_hess_binary(y_preds, sensitive_attribute)
-
-    for group in classes:
-        s_ovr = np.where(
-            sensitive_attribute == group, 0, 1
-        )  # group (as 0) vs rest (as 1)
-        hess_ovr = w2_fair_hess_binary(y_preds, s_ovr)
-        idx_group = np.where(sensitive_attribute == group)[0]
-        hess[idx_group] = hess_ovr[idx_group]
 
     return hess
 
